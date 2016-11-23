718ce878112da90918b52afe15356000791a92ef
Pawel Rynarzewski <pawel.rynarzewski@cumulocity.com>
2016-11-23 16:05:53 +0100

    TICKET-10943 Fix getting measurements

--- app/script/main.js
+++ app/script/main.js
@@ -424,7 +424,7 @@
         measurements.push(measurement);
       });
       if (res.measurements.length) {
-        getMeasurementsFromUrl(measurements, res.next.replace('http', 'https'))
+        getMeasurementsFromUrl(measurements, res.next.replace('http:', 'https:'))
           .then(function () {
             deferred.resolve(measurements);
           });
